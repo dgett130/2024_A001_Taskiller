410287c7920b625571acd07fa75c51eaa2c14df8
d.gettatelli <diego.gettatelli@alfameg.com>
2024-05-15 20:19:41 +0200

    init: first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+      <div className="app">
+          <div className="container border-red">
+          </div>
+          <div className="tool-container border-blue">
+          </div>
+          {/*<div className="container border-green">*/}
+          {/*</div>*/}
+          {/*<div className="container border-yellow">*/}
+          {/*</div>*/}
+      </div>
+  )
+}
+
+export default App
+
+
+
